7f5d32f696eb583f4b68c3fea8f29905d3a0d1df
shakerAbuDrais <thecoffeiner@gmail.com>
2022-09-28 00:32:28 +0300

    Add the structur for ToDo list app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const tasks = [
+  {
+    description: 'Clean the house',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Start working on JS',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Take a break',
+    completed: false,
+    index: 2,
+  },
+];
+
+const form = document.querySelector('.form');
+const toDoItem = document.createElement('div');
+const btn = document.createElement('p');
+btn.classList.add('btn');
+btn.innerHTML = 'Clear All Completed';
+
+tasks.forEach((el) => {
+  window.addEventListener('load', (() => {
+    toDoItem.innerHTML += `<input type="checkbox" id= ${el.index} name="item${el.index}" value="">
+      <label for="item${el.index}"> ${el.description}</label><span id="dots">&#8942;</span><br> <hr>`;
+    form.appendChild(toDoItem);
+    form.appendChild(btn);
+  }));
+});
